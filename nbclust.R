1941e629db0d1f4e89b7ccf93b075af36f667777
Mikewell <fff_khhh@mail.ru>
2021-09-16 17:52:59 +0700

    update

--- nbclust.R
+++ nbclust.R
@@ -7,3 +7,15 @@ data_B = read.csv(file='input/quotient_deviation_df_B_CLASSIFIED.csv', sep=";", 
 res_A <- NbClust(data_A, min.nc = 3, max.nc = 20, method = "kmean")
 
 res_B <- NbClust(data_B, min.nc = 3, max.nc = 20, method = "kmean")
+
+data_A_Diam = read.csv(file='input/d_cwt_separately/A_Diam.csv')
+data_A_CWT = read.csv(file='input/d_cwt_separately/A_CWT.csv')
+data_B_Diam = read.csv(file='input/d_cwt_separately/B_Diam.csv')
+data_B_CWT = read.csv(file='input/d_cwt_separately/B_CWT.csv')
+
+
+res_A_Diam <- NbClust(data_A_Diam, min.nc = 2, max.nc = 20, method = "kmean")
+res_A_CWT <- NbClust(data_A_CWT, min.nc = 2, max.nc = 20, method = "kmean")
+res_B_Diam <- NbClust(data_B_Diam, min.nc = 2, max.nc = 20, method = "kmean")
+res_B_CWT <- NbClust(data_B_CWT, min.nc = 2, max.nc = 20, method = "kmean")
+
