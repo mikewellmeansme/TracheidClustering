0200b7e30bd1d0e153b055a0f19031c32d3cbc04
Mikhail <fff_khhh@mail.ru>
2021-12-28 01:05:38 +0700

    add PDSI and SPEI

--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 from scipy.stats import pearsonr, spearmanr
+
 
 def dropna(x,y):
     x, y = np.array(x), np.array(y)
@@ -7,15 +9,18 @@ def dropna(x,y):
     x, y = x[~nas], y[~nas]
     return x, y
 
+
 def dropna_pearsonr(x, y):
     x, y = dropna(x,y)
     r, p = pearsonr(x, y)
     return r, p
 
+
 def dropna_spearmanr(x, y):
     x, y = dropna(x,y)
     r, p = spearmanr(x, y)
     return r, p
+
 
 def get_normalized_list(x: list, norm : int):
     """
@@ -33,3 +38,27 @@ def get_normalized_list(x: list, norm : int):
     for i in range(norm):
         l_norm += [1 / n * sum([l_raw[j] for j in range(n * i, n * (i + 1))])]
     return l_norm
+
+
+def running_mean(x, N=21):
+    cumsum = np.cumsum(np.insert(x, 0, 0)) 
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+
+def df_calculate_pvalues(df, method='pearson'):
+    """
+    Функция вычисления p-value для корреляций DataFrame'а df.
+    Поддерживает method pearson и spearman
+    """
+    df = df.dropna()._get_numeric_data()
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how='outer')
+    for r in df.columns:
+        for c in df.columns:
+            if method == 'pearson':
+                pvalues[r][c] = dropna_pearsonr(df[r], df[c])[1]
+            elif method == 'spearnan':
+                pvalues[r][c] = dropna_spearmanr(df[r], df[c])[1]
+            else:
+                raise Exception('Wrong correlation method!')
+    return pvalues
