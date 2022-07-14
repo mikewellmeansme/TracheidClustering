95783f73f72959087e7791d3134a83956b042b93
Mikhail <fff_khhh@mail.ru>
2022-07-14 16:29:57 +0700

    refactor: extract class climate index

---
+++ app/climate_indexes/climate_index.py
@@ -0,0 +1,11 @@
+from pandas import DataFrame, read_csv
+
+
+class ClimateIndex:
+    name: str
+    climate_index: DataFrame
+
+
+    def __init__(self, name: str, path: str) -> None:
+        self.name = name
+        self.climate_index = read_csv(path)
