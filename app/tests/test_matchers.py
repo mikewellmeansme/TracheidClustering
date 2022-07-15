993edb71a104bc2283185123a0b3f21b5b255b99
Mikhail <fff_khhh@mail.ru>
2022-07-16 00:27:08 +0700

    refactor: test move dataframes

--- app/tests/test_matchers.py
+++ app/tests/test_matchers.py
@@ -1,14 +1,19 @@
+import os
 import pytest
 import matchers
+
 from pandas import DataFrame, read_csv
 
 monthly_matcher = matchers.MonthlyClimateIndexMatcher()
+path_001 = 'dataframes/test_matchers/get_shifted_df/'
+path_002 = 'dataframes/test_matchers/merge_with_classes/'
 
 
 @pytest.mark.parametrize(
     ('input_df', 'expected_df'),
     [
-        (read_csv('test_dataframes/df1.csv'), read_csv('test_dataframes/df2.csv'))
+        (read_csv(f'{path_001}/input_df/{i}'), read_csv(f'{path_001}/expected_df/{i}'))
+        for i in os.listdir(f'{path_001}/input_df')
     ]
 )
 def test_get_shifted_df(input_df: DataFrame, expected_df: DataFrame):
@@ -18,7 +23,8 @@ def test_get_shifted_df(input_df: DataFrame, expected_df: DataFrame):
 @pytest.mark.parametrize(
     ('input_df', 'classes_df', 'expected_df'),
     [
-        (read_csv('test_dataframes/df3.csv'), read_csv('test_dataframes/df1.csv'), read_csv('test_dataframes/df4.csv'))
+        (read_csv(f'{path_002}/input_df/{i}'), read_csv(f'{path_002}/classes_df/{i}'), read_csv(f'{path_002}/expected_df/{i}'))
+        for i in os.listdir(f'{path_002}/input_df')
     ]
 )
 def test_merge_with_classes(input_df, classes_df, expected_df):
